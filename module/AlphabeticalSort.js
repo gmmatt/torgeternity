e7d6a621c51c9db9b6089024af198103e11447d1
rwan <erwan_lemaire@yahoo.fr>
2021-03-18 02:28:28 +0100

    sorting attributs

--- module/AlphabeticalSort.js
+++ module/AlphabeticalSort.js
@@ -2,8 +2,8 @@
 
 
 export function alphabSort() {
-    const lists = document.getElementsByClassName("skill-list");
-    for (let list of lists) {
+    const skillLists = document.getElementsByClassName("skill-list");
+    for (let list of skillLists) {
         const li = list.childNodes;
         let complist = [];
         for (let sk of li) {
@@ -14,6 +14,24 @@ export function alphabSort() {
         complist.sort(function(a, b) {
            if ( a.firstElementChild.firstElementChild && b.firstElementChild.firstElementChild){
             return (a.firstElementChild.firstElementChild.innerHTML > b.firstElementChild.firstElementChild.innerHTML) ? 1 : -1;
+           }
+        });
+        for (let sk of complist) {
+            list.appendChild(sk)
+        }
+    }
+    const attList = document.getElementsByClassName("attribut-list");
+    for (let list of attList) {
+        const li = list.childNodes;
+        let complist = [];
+        for (let sk of li) {
+            if (sk.tagName == "LI") {
+                complist.push(sk);
+            }
+        }
+        complist.sort(function(a, b) {
+           if ( a.firstElementChild && b.firstElementChild){
+            return (a.firstElementChild.innerHTML > b.firstElementChild.innerHTML) ? 1 : -1;
            }
         });
         for (let sk of complist) {
