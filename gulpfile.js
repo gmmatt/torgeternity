1c86a8ed042d53645cccfd8749f614dfb3a52d3e
rwan <erwan_lemaire@yahoo.fr>
2021-03-23 07:56:06 +0100

    cleaning scss compile

--- gulpfile.js
+++ gulpfile.js
@@ -12,7 +12,7 @@ function handleError(err) {
   this.emit('end');
 }
 
-const SYSTEM_SCSS = ["scss/**/*.scss"];
+const SYSTEM_SCSS = ["scss/*.scss"];
 
 function compileScss() {
   // Configure options for sass output. For example, 'expanded' or 'nested'
