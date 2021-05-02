61b61613c001413f1fe28ce3b8cfdc1e81fa7972
Rwan <erwan_lemaire@yahoo.fr>
2021-05-03 00:44:58 +0200

    better bar colors

--- module/tokenBars.js
+++ module/tokenBars.js
@@ -6,7 +6,7 @@ export function modifyTokenBars() {
         let h = Math.max((canvas.dimensions.size / 12), 8);
         if (this.data.height >= 2) h *= 1.6;  // Enlarge the bar for large tokens
         // Draw the bar
-        let color = [Math.clamped((pct * 1.3),0,1), 0.5 - (pct * 0.5), (pct * 0.1)];
+        let color = [0.8, (1 - pct), (0)];
         bar.clear()
             .beginFill(0x009900, 1)
             .lineStyle(1, 0x000000, 1)
