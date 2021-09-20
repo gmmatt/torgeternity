dec46c576c9e11e271f5082fe225b98601af5126
Matt Ritchie <matteritchie@gmail.com>
2021-09-19 21:23:49 -0500

    cosmetic updates to various parts of interface

--- module/settings.js
+++ module/settings.js
@@ -6,7 +6,7 @@ export function registerTorgSettings() {
     game.settings.register("torgeternity", "welcomeMessage", {
         // game.setting.register("NameOfTheModule", "VariableName",
         name: "display the welcome message", // Register a module setting with checkbox
-        hint: "If checked, the welcome message will pop once your world loaded", // Description of the settings
+        hint: "If checked, the welcome message will pop up after your world loads.", // Description of the settings
         scope: "world", // This specifies a client-stored setting
         config: true, // This specifies that the setting appears in the configuration view
         type: Boolean,
@@ -18,7 +18,7 @@ export function registerTorgSettings() {
     game.settings.register("torgeternity", "pauseMedia", {
         // game.setting.register("NameOfTheModule", "VariableName",
         name: "pause image media", // Register a module setting with checkbox
-        hint: "choose the image displayed when the game is paused", // Description of the settings
+        hint: "Choose the image that is displayed when the game is paused.", // Description of the settings
         type: window.Azzu.SettingsTypes.FilePickerImage,
         default: 'systems/torgeternity/images/pause.webp',
         scope: 'world',
@@ -31,7 +31,7 @@ export function registerTorgSettings() {
     game.settings.register("torgeternity", "animatedChat", {
         // game.setting.register("NameOfTheModule", "VariableName",
         name: "chat card animation", // Register a module setting with checkbox
-        hint: "If checked, enable chat card animations. changes will reload the app", // Description of the settings
+        hint: "If checked, enable chat card animations. Changing this setting will reload the app", // Description of the settings
         scope: "world", // This specifies a client-stored setting
         config: true, // This specifies that the setting appears in the configuration view
         type: Boolean,
