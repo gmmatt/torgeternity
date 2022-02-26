a2f32fdfc7e87791626ab3486752c6719ed91851
Erwan LEMAIRE <erwan_lemaire@yahoo.fr>
2022-02-26 07:02:15 +0100

    allowing localized cosm tent cards

--- module/torgeternityNav.js
+++ module/torgeternityNav.js
@@ -1,5 +1,8 @@
 export default class TorgeternityNav extends SceneNavigation {
+    getData() {
+        return mergeObject(super.getData(), { lang: game.settings.get("core", "language") })
+    }
     get template() {
         return "systems/torgeternity/templates/scenes/nav.hbs";
-      }
     }
+}
