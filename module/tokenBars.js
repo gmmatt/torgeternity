ebec9a681a351c227dcf074a956618c41d993f53
Erwan LEMAIRE <erwan_lemaire@yahoo.fr>
2022-02-26 16:37:29 +0100

    customizing tokens effects size

--- module/tokenBars.js
+++ module/tokenBars.js
@@ -24,5 +24,55 @@ export function modifyTokenBars() {
         let posY = number === 0 ? this.h - h : 0;
         bar.position.set(0, posY);
     }
-
+    Token.prototype._drawEffect=async function (src, i, bg, w, tint) {
+        let tex = await loadTexture(src, {fallback: 'icons/svg/hazard.svg'});
+        let icon = this.hud.effects.addChild(new PIXI.Sprite(tex));
+        icon.width = icon.height = w;
+        const nr = Math.floor(this.data.height * 3);
+        icon.x = Math.floor(i / nr) * w*-1;
+        icon.y = (i % nr) * w;
+        if ( tint ) icon.tint = tint;
+        bg.drawRoundedRect(icon.x + 1, icon.y + 1, w - 2, w - 2, 2);
+      }
+    
+    Token.prototype.drawEffects=async function () {
+        this.hud.effects.removeChildren().forEach(c => c.destroy());
+        const tokenEffects = this.data.effects;
+        const actorEffects = this.actor?.temporaryEffects || [];
+        let overlay = {
+          src: this.data.overlayEffect,
+          tint: null
+        };
+    
+        // Draw status effects
+        if ( tokenEffects.length || actorEffects.length ) {
+          const promises = [];
+          let w = Math.round(canvas.dimensions.size / 2 / 3) * 2;
+          let bg = this.hud.effects.addChild(new PIXI.Graphics()).beginFill(0x000000, 0.40).lineStyle(1.0, 0x000000);
+          let i = 0;
+    
+          // Draw actor effects first
+          for ( let f of actorEffects ) {
+            if ( !f.data.icon ) continue;
+            const tint = f.data.tint ? foundry.utils.colorStringToHex(f.data.tint) : null;
+            if ( f.getFlag("core", "overlay") ) {
+              overlay = {src: f.data.icon, tint};
+              continue;
+            }
+            promises.push(this._drawEffect(f.data.icon, i, bg, w, tint));
+            i++;
+          }
+    
+          // Next draw token effects
+          for ( let f of tokenEffects ) {
+            promises.push(this._drawEffect(f, i, bg, w, null));
+            i++;
+          }
+          await Promise.all(promises);
+        }
+    
+        // Draw overlay effect
+        return this._drawOverlay(overlay)
+      }
+    
 }
