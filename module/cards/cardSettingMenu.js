49f8bedf0f127aa20115001f0a639095ed2db376
Erwan LEMAIRE <erwan_lemaire@yahoo.fr>
2022-04-03 09:04:00 +0200

    controling doubled deck values in  setting menu

--- module/cards/cardSettingMenu.js
+++ module/cards/cardSettingMenu.js
@@ -15,7 +15,6 @@ export default class deckSettingMenu extends FormApplication {
     }
 
     getData() {
-
         let data = {
             deckList: game.cards.contents,
             object: game.settings.get("torgeternity", "deckSetting")
@@ -33,4 +32,33 @@ export default class deckSettingMenu extends FormApplication {
         const data = expandObject(formData);
         game.settings.set('torgeternity', 'deckSetting', data);
     }
+    onChangeDeck(html, event) {
+        //getting selected value
+        let selectedDeck = event.currentTarget.options[event.currentTarget.selectedIndex].value;
+
+        // checking if other select/options have same value
+        for (let option of html.find("option")) {
+            if (option.value == selectedDeck && option.closest("select").name != event.currentTarget.getAttribute("name")) {
+                if (option.selected) {
+                    option.closest("select").classList.add("doubled");
+                }
+            }
+        }
+        //checking if other select are not doubled anymore
+        for (let select of html.find("select")) {
+            if (select.options[select.selectedIndex].value != selectedDeck && select.classList.contains("doubled")) {
+                select.classList.remove("doubled")
+            }
+        }
+
+        //allowing submit if no doubled value
+        if (this.element.find(".doubled").length > 0) {
+            this.element.find('button[type="submit"]')[0].disabled = true
+        } else {
+            this.element.find('button[type="submit"]')[0].disabled = false
+
+        }
+    }
+
+
 }
