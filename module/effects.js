d9d2cc5ab189d2a8de1cbe9757bb66e8de030b48
rwanoux <erwan_lemaire@yahoo.fr>
2022-07-19 17:43:31 +0200

    translatable active effects labels

--- module/effects.js
+++ module/effects.js
@@ -1,53 +1,54 @@
 export function onManageActiveEffect(event, owner) {
-    event.preventDefault();
-    const a = event.currentTarget;
-    const li = a.closest("li");
-    const effect = li.dataset.effectId ? owner.effects.get(li.dataset.effectId) : null;
-    switch ( a.dataset.action ) {
-      case "create":
-        return owner.createEmbeddedDocuments("ActiveEffect", [{
-          label: "New Effect",
-          icon: "icons/svg/aura.svg",
-          origin: owner.uuid,
-          "duration.rounds": li.dataset.effectType === "temporary" ? 1 : undefined,
-          disabled: li.dataset.effectType === "inactive"
-        }]);
-      case "edit":
-        return effect.sheet.render(true);
-      case "delete":
-        return effect.delete();
-      case "toggle":
-        return effect.update({disabled: !effect.data.disabled});
-    }
+  event.preventDefault();
+  const a = event.currentTarget;
+  const li = a.closest("li");
+  const effect = li.dataset.effectId ? owner.effects.get(li.dataset.effectId) : null;
+  switch (a.dataset.action) {
+    case "create":
+      return owner.createEmbeddedDocuments("ActiveEffect", [{
+        label: "New Effect",
+        icon: "icons/svg/aura.svg",
+        origin: owner.uuid,
+        "duration.rounds": li.dataset.effectType === "temporary" ? 1 : undefined,
+        disabled: li.dataset.effectType === "inactive"
+      }]);
+    case "edit":
+      return effect.sheet.render(true);
+    case "delete":
+      return effect.delete();
+    case "toggle":
+      return effect.update({ disabled: !effect.data.disabled });
   }
-  
+}
+
 
 export function prepareActiveEffectCategories(effects) {
-    // Define effect header categories
-    const categories = {
-        temporary: {
-        type: "temporary",
-        label: `${game.i18n.localize("torgeternity.sheetLabels.tempEffects")}`,
-        effects: []
-        },
-        passive: {
-        type: "passive",
-        label: "Passive Effects",
-        effects: []
-        },
-        inactive: {
-        type: "inactive",
-        label: "Inactive Effects",
-        effects: []
-        }
-    };
+  // Define effect header categories
+  const categories = {
+    temporary: {
+      type: "temporary",
+      label: `${game.i18n.localize("torgeternity.sheetLabels.tempEffects")}`,
+      effects: []
+    },
+    passive: {
+      type: "passive",
+      label: `${game.i18n.localize("torgeternity.sheetLabels.passiveEffects")}`,
+      effects: []
+    },
+    inactive: {
+      type: "inactive",
+      label: "Inactive Effects", label: `${game.i18n.localize("torgeternity.sheetLabels.inactiveEffects")}`,
 
-    // Iterate over active effects, classifying them into categories
-    for ( let e of effects ) {
-        e._getSourceName(); // Trigger a lookup for the source name
-        if ( e.data.disabled ) categories.inactive.effects.push(e);
-        else if ( e.isTemporary ) categories.temporary.effects.push(e);
-        else categories.passive.effects.push(e);
+      effects: []
     }
-    return categories; 
+  };
+
+  // Iterate over active effects, classifying them into categories
+  for (let e of effects) {
+    e._getSourceName(); // Trigger a lookup for the source name
+    if (e.data.disabled) categories.inactive.effects.push(e);
+    else if (e.isTemporary) categories.temporary.effects.push(e);
+    else categories.passive.effects.push(e);
+  }
+  return categories;
 }
