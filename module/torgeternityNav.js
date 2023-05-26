65cd2fd8770e9f2fefadd87d40a3d5315cbbe1d4
Matt Ritchie <matteritchie@gmail.com>
2023-05-26 15:35:25 -0500

    update navigation for v11

--- module/torgeternityNav.js
+++ module/torgeternityNav.js
@@ -1,6 +1,9 @@
 export default class TorgeternityNav extends SceneNavigation {
     getData() {
-        return mergeObject(super.getData(), { lang: game.settings.get("core", "language") })
+     let context = super.getData()
+     context.lang = game.settings.get("core", "language");
+     context.scenes = context.scenes.map(s => ({...s, flags: game.scenes.get(s.id).flags}))
+     return context
     }
     get template() {
         return "systems/torgeternity/templates/scenes/nav.hbs";
