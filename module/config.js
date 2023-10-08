09f7a2dff9663e9df92ee4a13ef2372e9147b4ba
Durak-Git <karak_daoc@club-internet.fr>
2023-10-08 23:28:51 +0200

    Threat sheet
    Create a drop down menu, to choose possibility potential
    Avoid filling threat's possibilities at wrong place

--- module/config.js
+++ module/config.js
@@ -53,10 +53,6 @@ torgeternity.cosmTypes = {
     orrorsh: "torgeternity.cosms.orrorsh",
     other: "torgeternity.cosms.other"
 }
-torgeternity.actionLawCosms = {
-    nileEmpire: "torgeternity.cosms.nileEmpire",
-    other: "torgeternity.cosms.other"
-}
 
 torgeternity.actionLawCosms = {
     nileEmpire: "torgeternity.cosms.nileEmpire",
@@ -184,6 +180,13 @@ torgeternity.clearances = {
     gamma: "torgeternity.clearances.gamma",
     delta: "torgeternity.clearances.delta",
     omega: "torgeternity.clearances.omega"
+}
+
+torgeternity.threatPossibility = {
+    never: "torgeternity.threatPossibility.never",
+    rare: "torgeternity.threatPossibility.rare",
+    common: "torgeternity.threatPossibility.common",
+    unique: "torgeternity.threatPossibility.unique"
 }
 
 torgeternity.races = {
