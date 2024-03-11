cc0e55028db2c86b2ee382accc322af3f2c7fd1c
Gunnar Busch <gunnar.busch@googlemail.com>
2024-03-11 13:27:06 +0100

    add controlButtons back in!

---
+++ module/controlButtons.js
@@ -0,0 +1,61 @@
+import DeckSettingMenu from "./cards/cardSettingMenu.js";
+
+/**
+ *
+ */
+export default function initTorgControlButtons() {
+  // adding layer control for Torg entities
+  /**
+   *
+   */
+  CONFIG.Canvas.layers.torgeternity = { layerClass: ControlsLayer, group: "primary" };
+
+  Hooks.on("getSceneControlButtons", (btns) => {
+    const menu = [
+      {
+        name: game.i18n.localize("CARDS.TypeHand"),
+        title: game.i18n.localize("CARDS.TypeHand"),
+        icon: "fa fa-id-badge",
+        button: true,
+        onClick: () => {
+          if (game.user.character) {
+            game.user.character.getDefaultHand().sheet.toggleRender();
+          } else {
+            ui.notifications.error(game.i18n.localize("torgeternity.notifications.noHands"));
+          }
+        },
+      },
+    ];
+
+    if (game.user.isGM) {
+      menu.push(
+        {
+          name: game.i18n.localize("torgeternity.gmScreen.toggle"),
+          title: game.i18n.localize("torgeternity.gmScreen.toggle"),
+          icon: "fa fa-book-open",
+          button: true,
+          onClick: () => {
+            ui.GMScreen.toggleRender();
+          },
+        },
+        {
+          name: game.i18n.localize("torgeternity.settingMenu.deckSetting.name"),
+          title: game.i18n.localize("torgeternity.settingMenu.deckSetting.name"),
+          icon: "fa fa-cog",
+          button: true,
+          onClick: () => {
+            new DeckSettingMenu().render(true);
+          },
+        }
+      );
+    }
+
+    btns.push({
+      name: "TORG",
+      title: "TORG",
+      icon: "torg",
+      layer: "torgeternity",
+      tools: menu,
+    });
+  });
+}
