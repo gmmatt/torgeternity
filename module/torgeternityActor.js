1ea1d69125ac64eba9819aeff33c23ae3629d5da
Gunnar Busch <gunnar.busch@googlemail.com>
2024-03-11 16:47:13 +0100

    fix NaN display on new actors

--- module/torgeternityActor.js
+++ module/torgeternityActor.js
@@ -203,7 +203,7 @@ export default class torgeternityActor extends Actor {
 
     if ((this._source.type === "stormknight") | (this._source.type === "threat")) {
       for (const [name, skill] of Object.entries(skillset)) {
-        if (this._source.system.skills[name].adds === null) {
+        if (!this._source.system.skills[name].adds) {
           if (skill.unskilledUse === 1) {
             skill.value =
               parseInt(this.system.attributes[skill.baseAttribute]) + (skill.adds ? parseInt(skill.adds) : 0);
