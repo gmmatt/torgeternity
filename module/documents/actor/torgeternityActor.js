befc6c9fd3503bc1f3cfd1ca09580e9efea96e9f
Gunnar Busch <gunnar.busch@googlemail.com>
2024-03-24 09:53:39 +0100

    nah, don't write that straight to the actor.

--- module/documents/actor/torgeternityActor.js
+++ module/documents/actor/torgeternityActor.js
@@ -20,8 +20,6 @@ export default class TorgeternityActor extends Actor {
       // initialize the worn armor bonus
       this.system.other.armor = this.wornArmor?.system?.bonus ?? 0;
       this.system.fatigue = 2 + this.wornArmor?.system?.fatigue ?? 0;
-      this.maxDex = this.wornArmor?.system?.maxDex ?? null;
-      this.minStrength = this.wornArmor?.system?.minStrength ?? null;
     }
   }
 
