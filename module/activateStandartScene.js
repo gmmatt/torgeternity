4fd78fdb2f70be06581c590979fde9bd9f09ca58
Gunnar Busch <gunnar.busch@googlemail.com>
2024-03-24 10:44:04 +0100

    unpack compendia

--- module/activateStandartScene.js
+++ module/activateStandartScene.js
@@ -5,7 +5,9 @@ export async function activateStandartScene() {
   const pack = game.packs.get("torgeternity.core-scenes");
   const sceneId = pack.index.getName("Main scene")._id;
   game.scenes.importFromCompendium(pack, sceneId).then((s) => {
-    await s.update({ name: game.i18n.localize("torgeternity.scenes.main") || "Main Scene" });
-    s.activate();
+    s.update({
+      name: game.i18n.localize("torgeternity.scenes.main") || "Main Scene",
+      active: true,
+    });
   });
 }
