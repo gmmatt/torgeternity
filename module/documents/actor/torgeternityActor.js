14f60a38ebe873078a49f93a76c378cbc7273322
Gunnar Busch <gunnar.busch@googlemail.com>
2024-06-12 18:16:07 +0200

    shorten getDefaultHand by one document query

--- module/documents/actor/torgeternityActor.js
+++ module/documents/actor/torgeternityActor.js
@@ -202,12 +202,12 @@ export default class TorgeternityActor extends Actor {
   }
 
   /**
-   * @returns {object} the Hand of the actor
+   * @returns {object|false} the Hand of the actor or false if no default hand is set
    */
   getDefaultHand() {
-    const handId = game.cards.find((c) => c.flags?.torgeternity?.defaultHand === this.id)?.id;
-    if (handId) {
-      return game.cards.get(handId);
+    const hand = game.cards.find((c) => c.flags?.torgeternity?.defaultHand === this.id);
+    if (hand) {
+      return game.cards.get(hand);
     } else {
       console.error(`no default hand for actor : ${this.name}`);
       return false;
