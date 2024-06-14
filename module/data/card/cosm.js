14e7eea6f05c78203550f9b66730b2432815c2d8
Gunnar Busch <gunnar.busch@googlemail.com>
2024-06-14 12:33:15 +0200

    Create card data models, remove template.json

---
+++ module/data/card/cosm.js
@@ -0,0 +1,30 @@
+const fields = foundry.data.fields;
+/**
+ * @inheritdoc
+ */
+export class CosmCardData extends foundry.abstract.TypeDataModel {
+  /**
+   *
+   * @returns {object} Schema fragment for a Storm Knight or Threat
+   */
+  static defineSchema() {
+    return {
+      cosm: new fields.StringField({ initial: '' }),
+      number: new fields.NumberField({ initial: 1, integer: true }),
+    };
+  }
+
+  /**
+   * @inheritdoc
+   */
+  prepareBaseData() {
+    super.prepareBaseData();
+  }
+
+  /**
+   * @inheritdoc
+   */
+  prepareDerivedData() {
+    super.prepareDerivedData();
+  }
+}
---
+++ module/data/card/drama.js
@@ -0,0 +1,39 @@
+const fields = foundry.data.fields;
+/**
+ * @inheritdoc
+ */
+export class DramaCardData extends foundry.abstract.TypeDataModel {
+  /**
+   *
+   * @returns {object} Schema fragment for a Storm Knight or Threat
+   */
+  static defineSchema() {
+    return {
+      approvedActions: new fields.StringField({ initial: '' }),
+      cosm: new fields.StringField({ initial: '' }),
+      dsrLine: new fields.StringField({ initial: '' }),
+      heroesConditionsDramatic: new fields.StringField({ initial: '' }),
+      heroesConditionsStandard: new fields.StringField({ initial: '' }),
+      heroesFirstDramatic: new fields.BooleanField({ initial: true }),
+      heroesFirstStandard: new fields.BooleanField({ initial: true }),
+      number: new fields.NumberField({ initial: 1, integer: true }),
+      rule: new fields.StringField({ initial: '' }),
+      villainsConditionsDramatic: new fields.StringField({ initial: '' }),
+      villainsConditionsStandard: new fields.StringField({ initial: '' }),
+    };
+  }
+
+  /**
+   * @inheritdoc
+   */
+  prepareBaseData() {
+    super.prepareBaseData();
+  }
+
+  /**
+   * @inheritdoc
+   */
+  prepareDerivedData() {
+    super.prepareDerivedData();
+  }
+}
