ce6404bd18e9b2ab179c368b4046c1f72dd7d995
Algor0113 <80172941+Algor0113@users.noreply.github.com>
2024-07-20 09:53:25 +0200

    #131 rc01

--- module/sheets/torgeternityActorSheet.js
+++ module/sheets/torgeternityActorSheet.js
@@ -157,6 +157,7 @@ export default class TorgeternityActorSheet extends ActorSheet {
       'destinyCard',
       'cosmCard',
       'vehicleAddOn',
+      'ammunition',
     ]) {
       for (const item of data[type]) {
         item.description = await TextEditor.enrichHTML(item.system.description, {
