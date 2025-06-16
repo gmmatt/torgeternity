fdf80dd6b4a8350177f3f70df58153d6b0bdb4e4
farling42 <github@amusingtime.uk>
2025-06-16 13:30:18 +0100

    Scene Config Cosm tab fixed

--- module/torgeternitySceneConfig.js
+++ module/torgeternitySceneConfig.js
@@ -4,73 +4,89 @@ import { torgeternity } from './config.js';
  *
  */
 export default class torgeternitySceneConfig extends foundry.applications.sheets.SceneConfig {
-  getData(options = {}) {
-    const context = super.getData(options);
 
-    context.zones = torgeternity.zones;
+  static PARTS = {
+    tabs: { template: "templates/generic/tab-navigation.hbs" },
+    basics: { template: "templates/scene/config/basics.hbs" },
+    cosm: { template: `systems/torgeternity/templates/scenes/scenes-config.hbs` },
+    grid: { template: "templates/scene/config/grid.hbs" },
+    lighting: { template: "templates/scene/config/lighting.hbs", scrollable: [""] },
+    ambience: { template: "templates/scene/config/ambience.hbs", scrollable: ["div.tab[data-tab=environment]"] },
+    footer: { template: "templates/generic/form-footer.hbs" }
+  };
 
-    context.cosmTypes = torgeternity.cosmTypes;
-
-    return context;
-  }
-
-  /**
-   *
-   */
-  get template() {
-    // modified path => one folder per type
-    return `systems/torgeternity/templates/scenes/scenes-config.hbs`;
-  }
-  /**
-   *
-   * @param html
-   */
-  activateListeners(html) {
-    super.activateListeners(html);
-
-    const selCosm = html.find('select.cosm')[0];
-    selCosm.addEventListener('change', this._onChangeCosm.bind(this));
-    const selZone = html.find('select.zone-type')[0];
-    selZone.addEventListener('change', this._onChangeZone.bind(this));
-
-    const selCosm2 = html.find('select.cosm-secondary')[0];
-    if (selCosm2) {
-      selCosm2.addEventListener('change', this._onChangeCosm2.bind(this));
+  static TABS = {
+    sheet: {
+      tabs: [
+        { id: "basics", icon: "fa-solid fa-image" },
+        { id: "cosm", icon: "fa-solid fa-globe", label: "torgeternity.sheetLabels.cosm" },
+        { id: "grid", icon: "fa-solid fa-grid" },
+        { id: "lighting", icon: "fa-solid fa-lightbulb" },
+        { id: "ambience", icon: "fa-solid fa-cloud-sun" }
+      ],
+      initial: "basics",
+      labelPrefix: "SCENE.TABS.SHEET"
+    },
+    ambience: {
+      tabs: [
+        { id: "basic", icon: "fa-solid fa-table-list" },
+        { id: "environment", icon: "fa-solid fa-cloud-sun" }
+      ],
+      initial: "basic",
+      labelPrefix: "SCENE.TABS.AMBIENCE"
     }
+  }
+
+  async _preparePartContext(partId, context, options) {
+    const result = await super._preparePartContext(partId, context, options);
+    switch (partId) {
+      case "cosm":
+        context.zones = torgeternity.zones;
+        context.cosmTypes = torgeternity.cosmTypes;
+        break;
+    }
+    return result;
+  }
+
+  _onChangeForm(context, event) {
+    console.log("_onChangeForm", { context, event })
+    if (event.target.classList.contains("cosm"))
+      this.#onChangeCosm(event);
+    else if (event.target.classList.contains("zone-type"))
+      this.#onChangeZone(event);
+    else if (event.target.classList.contains("cosm-secondary"))
+      this.#onChangeCosm2(event);
   }
 
   /**
    *
    * @param ev
    */
-  _onChangeCosm(ev) {
-    const cosm = ev.currentTarget.options[ev.currentTarget.selectedIndex].value;
-    this.document.setFlag('torgeternity', 'cosm', cosm);
+  #onChangeCosm(ev) {
+    this.document.setFlag('torgeternity', 'cosm', ev.target.value);
   }
   /**
    *
    * @param ev
    */
-  _onChangeZone(ev) {
-    const zone = ev.currentTarget.options[ev.currentTarget.selectedIndex].value;
-    this.document.setFlag('torgeternity', 'zone', zone);
-    if (zone === 'mixed' || zone === 'dominant') {
-      this.document.setFlag('torgeternity', 'displayCosm2', true);
-    } else {
-      this.document.setFlag('torgeternity', 'displayCosm2', false);
-    }
-    if (zone === 'mixed') {
-      this.document.setFlag('torgeternity', 'isMixed', true);
-    } else {
-      this.document.setFlag('torgeternity', 'isMixed', false);
-    }
+  #onChangeCosm2(ev) {
+    this.document.setFlag('torgeternity', 'cosm2', ev.target.value);
   }
   /**
    *
    * @param ev
    */
-  _onChangeCosm2(ev) {
-    const cosm = ev.currentTarget.options[ev.currentTarget.selectedIndex].value;
-    this.document.setFlag('torgeternity', 'cosm2', cosm);
+  #onChangeZone(ev) {
+    const zone = ev.target.value;
+    // Quicker than three calls to this.document.setFlag
+    this.document.update({
+      flags: {
+        torgeternity: {
+          zone: zone,
+          displayCosm2: (zone === 'mixed' || zone === 'dominant'),
+          isMixed: (zone === 'mixed')
+        }
+      }
+    })
   }
 }
