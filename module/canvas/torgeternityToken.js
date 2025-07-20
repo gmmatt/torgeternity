55f6d681d0f857b45aecbe7dc2b0b864198cbe2e
farling42 <github@amusingtime.uk>
2025-07-20 01:35:00 +0100

    Touching tokens are 1m apart

--- module/canvas/torgeternityToken.js
+++ module/canvas/torgeternityToken.js
@@ -1,6 +1,8 @@
 export default class TorgEternityToken extends foundry.canvas.placeables.Token {
 
-  #label;
+  static #originToken;
+  static #hoverToken;
+  static #label;
 
   _onHoverIn(event, options) {
     const result = super._onHoverIn(event, options)
@@ -9,54 +11,62 @@ export default class TorgEternityToken extends foundry.canvas.placeables.Token {
 
     const originActor = game.user.character;
     if (!originActor) return result;
-    const originToken = canvas.scene.tokens.find(t => t.actor === originActor);
-    if (originToken.object === this) return;
-    const path = [originToken.object.center, this.center];
-    const measurement = canvas.grid.measurePath(path);
+    TorgEternityToken.#originToken = canvas.scene.tokens.find(t => t.actor === originActor);
+    if (TorgEternityToken.#originToken.object === this) return;
 
-    console.log(`show distance from ${originActor.name} to ${this.name}`, measurement)
-
-    // Create the label HTML
-    const distLabel = measurement.distance.toNearest(0.01).toLocaleString(game.i18n.lang);
-    let html = `<div class="waypoint-label token-distance last"><span class="total-measurement">${distLabel} ${canvas.grid.units}</span></div>`;
-    html = foundry.utils.parseHTML(html);
-    html.style.setProperty("--position-x", `${this.center.x}px`);
-    // 50 to get text ABOVE the top of the token
-    html.style.setProperty("--position-y", `${this.center.y - 0.5 * this.h - (50 * canvas.dimensions.uiScale)}px`);
-    html.style.setProperty("--ui-scale", canvas.dimensions.uiScale);
-    console.log({ context, html });
-    this.#getLabelElement().replaceChildren(html);
+    TorgEternityToken.#hoverToken = this;
+    Hooks.on('refreshToken', TorgEternityToken.drawLabel);
+    TorgEternityToken.drawLabel();
 
     // Ensure it is visible
-    this.#getLabelElement().classList.toggle("hidden", false);
-
+    TorgEternityToken.#getLabelElement().classList.toggle("hidden", false);
     return result;
   }
 
   _onHoverOut(event, options) {
     const result = super._onHoverIn(event, options);
-    if (!this.#label) return result;
+    if (!TorgEternityToken.#label) return result;
 
+    Hooks.off('refreshToken', TorgEternityToken.drawLabel);
     if (!game.settings.get('torgeternity', 'hoverDistance') || !this.visible) return result;
-    console.log(`hide distance from ${game.user.character.name} to ${this.name}`)
-    this.#getLabelElement().classList.toggle("hidden", true);
+    //console.log(`hide distance from ${game.user.character.name} to ${this.name}`)
+    TorgEternityToken.#getLabelElement().classList.toggle("hidden", true);
     return result;
   }
 
-  #getLabelElement() {
-    let label = this.#label;
+  static drawLabel() {
+    let distance = canvas.grid.measurePath([TorgEternityToken.#originToken.object.center, TorgEternityToken.#hoverToken.center]).distance;
+    // Account for distance needing to be from edge to edge of token, with touching tokens having a distance of 1.
+    distance -= (TorgEternityToken.#originToken.height + TorgEternityToken.#hoverToken.document.height) / 2 - 1;
+
+    //console.log(`show distance from ${TorgEternityToken.#originToken.name} to ${TorgEternityToken.#hoverToken.name}`, distance)
+
+    // Create the label HTML
+    const distLabel = distance.toNearest(0.01).toLocaleString(game.i18n.lang);
+    let html = `<div class="waypoint-label token-distance last"><span class="total-measurement">${distLabel} ${canvas.grid.units}</span></div>`;
+    html = foundry.utils.parseHTML(html);
+    html.style.setProperty("--position-x", `${TorgEternityToken.#hoverToken.center.x}px`);
+    // 50 to get text ABOVE the top of the token
+    html.style.setProperty("--position-y", `${TorgEternityToken.#hoverToken.center.y - 0.5 * TorgEternityToken.#hoverToken.h - (50 * canvas.dimensions.uiScale)}px`);
+    html.style.setProperty("--ui-scale", canvas.dimensions.uiScale);
+    //console.log({ context, html });
+    TorgEternityToken.#getLabelElement().replaceChildren(html);
+  }
+
+  static #getLabelElement() {
+    let label = TorgEternityToken.#label;
     if (!label) {
-      let rulerId = `token-distance-${this.document.id}`;
-      if (this.isPreview) rulerId += "-preview";
+      let rulerId = `token-distance-${TorgEternityToken.#hoverToken.document.id}`;
+      if (TorgEternityToken.#hoverToken.isPreview) rulerId += "-preview";
       label = document.querySelector(`#hud #measurement #${rulerId}`);
 
       if (!label) {
         label = document.createElement("div");
         label.classList.add("ruler-labels", "token-ruler-labels");
-        if (!this.visible) label.classList.add("hidden");
+        if (!TorgEternityToken.#hoverToken.visible) label.classList.add("hidden");
         label.id = rulerId;
       }
-      this.#label = label;
+      TorgEternityToken.#label = label;
     }
     if (label.parent) return label;
     document.querySelector("#hud #measurement")?.appendChild(label);
