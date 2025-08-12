3de66316a1c4b6c1d833722fc02eaaf9ac157c5f
farling42 <github@amusingtime.uk>
2025-08-12 22:24:57 +0100

    Remove commented-out code

--- module/torgeternity.js
+++ module/torgeternity.js
@@ -300,29 +300,8 @@ Hooks.once('diceSoNiceReady', (dice3d) => {
   registerDiceSoNice(dice3d);
 });
 
-/*
-Hooks.on("renderUIConfig", (config, html, context, options) => {
-  let select = html.querySelector('[name="core.uiConfig.colorScheme.applications');
-  if (select) {
-    select.disabled = true;
-    let hint = select.parentElement.parentElement.querySelector("p.hint");
-    if (hint) hint.innerText = game.i18n.localize("torgeternity.core.forceDarkModeHint");
-  }
-})
-  */
-
 // -------------once everything ready
 Hooks.on('ready', async function () {
-
-  /*
-  // Force DARK application colour scheme
-  const uiconfig = game.settings.get('core', 'uiConfig');
-  if (uiconfig.colorScheme.applications !== 'dark') {
-    //ui.notifications.warn('TORG system works best when the Applications theme is set to DARK');
-    uiconfig.colorScheme.applications = 'dark';
-    game.settings.set('core', 'uiConfig', uiconfig);
-  }
-    */
 
   // migration script
   if (game.user.isGM) torgMigration();
