648b8785b0c403642f94f02c13286fda7a5abc3b
farling42 <github@amusingtime.uk>
2025-09-12 15:06:00 +0100

    Create only one ResizeObserver for each Actor type

--- module/sheetResize.js
+++ module/sheetResize.js
@@ -1,38 +1,43 @@
 /**
  *
  */
+
+let ro_stormknight;
+let ro_threat;
+
 export function sheetResize() {
   Hooks.on('renderTorgeternityActorSheet', async function (sheet, element, context, options) {
     let ro;
     switch (sheet.document.type) {
       case 'stormknight':
-        ro = new ResizeObserver((entries) => {
+        if (!ro_stormknight) ro_stormknight = new ResizeObserver((entries) => {
+          console.log(`triggered`, entries);
           for (const entry of entries) {
             const cr = entry.contentRect;
             if (cr.width < 510 || cr.height < 650) {
-              element.classList.add('compact');
+              entry.target.classList.add('compact');
             } else {
-              element.classList.remove('compact');
+              entry.target.classList.remove('compact');
             }
           }
         });
-        ro.observe(element);
+        ro_stormknight.observe(element);
         break;
 
       case 'threat':
-        ro = new ResizeObserver((entries) => {
+        if (!ro_threat) ro_threat = new ResizeObserver((entries) => {
           for (const entry of entries) {
             const cr = entry.contentRect;
             if (cr.height < 630) {
-              element.classList.add('tabsOff');
-              element.classList.remove('tabsOn');
+              entry.target.classList.add('tabsOff');
+              entry.target.classList.remove('tabsOn');
             } else {
-              element.classList.remove('tabsOff');
-              element.classList.add('tabsOn');
+              entry.target.classList.remove('tabsOff');
+              entry.target.classList.add('tabsOn');
             }
           }
         });
-        ro.observe(element);
+        ro_threat.observe(element);
         break;
     }
   });
