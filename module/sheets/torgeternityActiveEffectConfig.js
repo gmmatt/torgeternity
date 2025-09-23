5810e74d1122a45849e6058d9936d42565c6cb13
farling42 <github@amusingtime.uk>
2025-09-23 11:38:21 +0100

    Move Torg AE fields to separate tab

--- module/sheets/torgeternityActiveEffectConfig.js
+++ module/sheets/torgeternityActiveEffectConfig.js
@@ -1,17 +1,43 @@
 export default class TorgActiveEffectConfig extends foundry.applications.sheets.ActiveEffectConfig {
 
+  /** @override */
   static PARTS = {
     header: { template: "templates/sheets/active-effect/header.hbs" },
     tabs: { template: "templates/generic/tab-navigation.hbs" },
-    details: { template: "systems/torgeternity/templates/active-effect-details.hbs", scrollable: [""] },
+    details: { template: "templates/sheets/active-effect/details.hbs", scrollable: [""] },
+    torg: { template: "systems/torgeternity/templates/active-effect-torg.hbs", scrollable: [""] },
     duration: { template: "templates/sheets/active-effect/duration.hbs" },
     changes: { template: "templates/sheets/active-effect/changes.hbs", scrollable: ["ol[data-changes]"] },
     footer: { template: "templates/generic/form-footer.hbs" }
   };
 
+  /** @override */
+  static TABS = {
+    sheet: {
+      tabs: [
+        { id: "details", icon: "fa-solid fa-book" },
+        { id: "torg", icon: "fa-solid fa-swords" },
+        { id: "duration", icon: "fa-solid fa-clock" },
+        { id: "changes", icon: "fa-solid fa-gears" }
+      ],
+      initial: "details",
+      labelPrefix: "EFFECT.TABS"
+    },
+  };
+
   async _prepareContext(options) {
     const context = await super._prepareContext(options);
-    context.systemFields = context.document.system.schema.fields;
+    if (context.document.parent.documentName !== "Item") {
+      delete context.tabs.torg;
+    }
     return context;
+  }
+
+  async _preparePartContext(partId, context) {
+    const partContext = await super._preparePartContext(partId, context);
+    if (partId === 'torg') {
+      partContext.systemFields = this.document.system.schema.fields;
+    }
+    return partContext;
   }
 }
